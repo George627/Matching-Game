dc921a1b03a8fbb7541ec0bfad164b04fc6eab4f
George627 <81629113+George627@users.noreply.github.com>
2023-09-01 20:36:03 -0400

    Update script.js
    Adding comments and changing the var to let.

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 //Grabs all card classes and places it into cards constant.
 const cards = document.querySelectorAll(".card");
 
+//Grabs the inner side element of the card.
 const innerColors = document.querySelectorAll(".inner-card");
 
+//The reset button.
 const reset = document.querySelector("button");
 
+//Array of colors for the game.
 const colors = ["red", "red", 
                 "blue", "blue", 
                 "yellow", "yellow", 
@@ -12,45 +15,51 @@ const colors = ["red", "red",
                 "purple", "purple",
                 "green", "green"];
 
-
+//The method that shuffles the colors.
 colorShuffle(shuffle(colors));
 
+/*A forEach method that takes all the card elements 
+and applies the clickable function to each of them.*/
 
 cards.forEach(clickableCards);
 
-
+/*An addEventListener that is linked the the reset button. 
+If clicked, (1) then it will reset the game by flipping all the cards over,
+(2) then shuffle the colors for each card. */
 reset.addEventListener("click", function () {
-    const one = () => {console.log(1)};
-    
-    const eight = setTimeout(one, 10000);
-
-  
+    //1
     cards.forEach(resetGame);
 
+    //2 
     colorShuffle(shuffle(colors));
 })
 
-
+//The clickableCards function takes the current card then incorporate the flip function to it. 
 function clickableCards(card){
    card.addEventListener("click", flip);
 }
 
-
+//The flip function takes a card and will either add or remove the flip class.
 function flip() {
-     
+
+    //Grabs the current card.
     let flip = this;  
-    
+
+    //If the current card does NOT contain the flip class, then add it.
     if(!flip.classList.contains("flip")){
-        
+
+        //Adds the flip class to the current card.
         flip.classList.add("flip");
     }
-    
+
+    //Else, remove the flip class.
     else{
         flip.classList.remove("flip");
     }
 
 }
 
+//The shuffle function takes in the array of colors, then shuffles it.
 function shuffle(color) {
     let currentIndex = color.length,  randomIndex;
     
@@ -65,18 +74,24 @@ function shuffle(color) {
         [color[currentIndex], color[randomIndex]] = [
         color[randomIndex], color[currentIndex]];
     }
-    
+
+    //Returns the new array of colors.
     return color;
 }
 
+/*The colorShuffle function takes in array of colors 
+and places them on the inner side of the card.*/
 function colorShuffle(newColors){
-    var shuffColors = newColors;
+    let shuffColors = newColors;
 
+    /*Using a for loop to loop through all inner cards
+    and adds a color to it.*/
     for(let i = 0; i < newColors.length; i++){
         innerColors[i].style.backgroundColor = shuffColors[i];
     }
 }
 
+//The resetGame functions flips all the cards over to their back side.
 function resetGame(card){
     card.classList.remove("flip");
 }
